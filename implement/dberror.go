1adb89e3ce9b8479b18132166956d57dae7721f8
hank <akysn564@gmail.com>
2022-04-11 14:21:26 +0800

    other Error message

--- implement/dberror.go
+++ implement/dberror.go
@@ -16,9 +16,10 @@ type Error struct {
 func ValidDbError(err error, ctx *gin.Context) bool {
 	var e mongo.WriteException
 	if errors.As(err, &e) {
+		out := make([]Error, 1)
 		for _, we := range e.WriteErrors {
 			if we.Message != "" && we.Code == 11000 {
-				out := make([]Error, 1)
+
 				if strings.Index(we.Message, "name_1") != -1 {
 					out[0] = Error{"名稱已被註冊過"}
 					ctx.JSON(http.StatusBadGateway, gin.H{"errors": out})
@@ -29,6 +30,9 @@ func ValidDbError(err error, ctx *gin.Context) bool {
 					out[0] = Error{we.Message}
 					ctx.JSON(http.StatusBadGateway, gin.H{"errors": out})
 				}
+			} else {
+				out[0] = Error{we.Message}
+				ctx.JSON(http.StatusBadGateway, gin.H{"errors": out})
 			}
 		}
 		return true
